99fb687f14727ba5a3cbf65a63267094336c6882
flatherskevin <flatherskevin@gmail.com>
2022-02-25 22:51:50 -0500

    Cleaning http session

--- tradebot/http.py
+++ tradebot/http.py
@@ -5,7 +5,13 @@ import requests
 from dotenv import load_dotenv
 
 
-def get_http_session():
-    env_vars = load_dotenv(Path.cwd() / Path("secrets", "api.env"))
+def get_http_session() -> requests.Session:
+    load_dotenv(Path.cwd() / Path("secrets", "api.env"))
     session = requests.Session()
-    session.headers.update({"APCA-API-KEY-ID": os})
+    session.headers.update(
+        {
+            "APCA-API-KEY-ID": os.environ["API_KEY_ID"],
+            "APCA-API-SECRET-KEY": os.environ["SECRET_KEY"],
+        }
+    )
+    return session
